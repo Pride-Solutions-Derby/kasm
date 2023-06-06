f1168e4710d0a77727152264ab6baace6eef0b22
Pride-Solutions-Derby <66773112+Pride-Solutions-Derby@users.noreply.github.com>
2023-06-06 14:25:54 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    name: 'Pride Solutions Derby Registry',
+    description: 'The official store for Pride Solutions Derby workspaces.',
+    icon: 'https://github.com/Pride-Solutions-Derby/kasm/1.0/icon.png',
+    listUrl: 'https://github.com/Pride-Solutions-Derby/kasm/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
